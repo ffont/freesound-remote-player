6070b241c06fa39fc45fca04ea278c6a34d6975b
ffont <frederic.font@gmail.com>
2016-11-23 11:06:23 +0100

    Update client

--- freesound-cli-player.py
+++ freesound-cli-player.py
@@ -6,11 +6,31 @@ HOST = '10.80.26.190'
 PORT = 3333
 BASE_URL = 'http://%s:%i/' % (HOST, PORT)
 PLAY_URL = BASE_URL + 'play'
+VOICE_URL = BASE_URL + 'voice'
+
+"""
+TODO: this should really be turned into a proper command line app
+For now you can either:
+    python freesound-cli-player.py tag1 tag2 tag3
+or:
+    python freesound-cli-player.py tag1,tag2,tag3
+for playing a random sound from Freesound with the given tags, or you can also
+    python freesound-cli-player.py read a text you want to be read
+to use system's 'say' command to read something. So basically if first argument is
+'read' then instead of playing a Freesound sound the server will read the folloing arguments.
+"""
 
 if __name__ == "__main__":
     args = sys.argv[1:]
     if len(args):
-        tags = ','.join(args)
-        url = PLAY_URL + '?tags=' + tags
+        if args[0] == 'read':
+            # Read following arguments
+            url = VOICE_URL + '?text="%s"' % ' '.join(args[1:])
+        else:
+            # Play fs sound with tags
+            tags = ','.join(args)
+            url = PLAY_URL + '?tags=' + tags
+        print url
         response = urllib.urlopen(url)
         print json.loads(response.read())
+        print ''
